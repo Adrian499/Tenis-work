b3df6c8bd035ae57a14a730ae7b4687ce35b7f3d
Adrian499 <90834688+Adrian499@users.noreply.github.com>
2024-11-13 23:19:56 +0000

    Update play-tennis.py
    Solo falta crear variables.

--- play-tennis.py
+++ play-tennis.py
@@ -1,14 +1,11 @@
 # CONSTANTES
     PLAYER_1 = 'A'
     PLAYER_2 = 'B'
-
     # variables
     pa_points = 0
     pb_points = 0
-    pa_sets = 0
-    pb_sets = 0
-    win_match_a = 0
-    win_match_b = 0
+    pa_games = 0
+    pb_games = 0
     pa_tie_break = 0
     pb_tie_break = 0
     result = ''
@@ -17,38 +14,55 @@
         # Sumando cada punto
         if set_game == PLAYER_1:
             pa_points += 1
-        elif set_game == PLAYER_2:
+        else:
             pb_points += 1
 
-        # Victoria fácil (15-50)
-        if pa_points >= 4 and pa_points - pb_points >= 2:
-            pa_sets += 1
-            pa_points = 0
-            pb_points = 0
-        elif pb_points >= 4 and pb_points - pa_points >= 2:
-            pb_sets += 1
-            pa_points = 0
-            pb_points = 0
-        # Filtro q si es empate o no
-        if pa_sets == 6 and pb_sets == 6:
+        # Filtro empate o no
+        if pa_games == 6 and pb_games == 6:
             if set_game == PLAYER_1:
                 pa_tie_break += 1
-
-            if set_game == PLAYER_2:
+            elif set_game == PLAYER_2:
                 pb_tie_break += 1
 
-        # Sets fácil (1-6)
-        if pa_sets >= 6 and pa_sets - pb_sets >= 2:
-            result += f'{pa_sets}-{pb_sets} '
-            win_match_a += 1
-            # Reseteo sets
-            pa_sets = 0
-            pb_sets = 0
-        elif pb_sets >= 6 and pb_sets - pa_sets >= 2:
-            result += f'{pa_sets}-{pb_sets} '
-            win_match_b += 1
-            # Reseteo sets
-            pa_sets = 0
-            pb_sets = 0
+            if pa_tie_break >= 7 and pa_tie_break - pb_tie_break >= 2:
+                pa_games += 1
+                result += f'{pa_games}-{pb_games} '
+                pa_games = 0
+                pb_games = 0
+                pa_tie_break = 0
+                pb_tie_break = 0
+                pa_points = 0
+                pb_points = 0
+            elif pb_tie_break >= 7 and pb_tie_break - pa_tie_break >= 2:
+                pb_games += 1
+                result += f'{pa_games}-{pb_games} '
+                pa_games = 0
+                pb_games = 0
+                pa_tie_break = 0
+                pb_tie_break = 0
+                pa_points = 0
+                pb_points = 0
+        else:
+            if pa_points >= 4 and pa_points - pb_points >= 2:
+                pa_games += 1
+                pa_points = 0
+                pb_points = 0
+                pb_tie_break = 0
+            elif pb_points >= 4 and pb_points - pa_points >= 2:
+                pb_games += 1
+                pa_points = 0
+                pb_points = 0
+                pb_tie_break = 0
+            # Sets fácil (1-6)
+            if pa_games >= 6 and pa_games - pb_games >= 2:
+                result += f'{pa_games}-{pb_games} '
+                # Reseteo sets
+                pa_games = 0
+                pb_games = 0
+            elif pb_games >= 6 and pb_games - pa_games >= 2:
+                result += f'{pa_games}-{pb_games} '
+                # Reseteo sets
+                pa_games = 0
+                pb_games = 0
 
     return result
